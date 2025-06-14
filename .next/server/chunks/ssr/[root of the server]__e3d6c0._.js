dc0bed4e83dd3a6c5f245c371b145291cc8b0f6b
Siva-kumar3 <147022359+Siva-kumar3@users.noreply.github.com>
2025-06-14 11:01:07 +0530

    push changes file

--- .next/server/chunks/ssr/[root of the server]__e3d6c0._.js
+++ .next/server/chunks/ssr/[root of the server]__e3d6c0._.js
@@ -1215,7 +1215,7 @@ const Hero = ()=>{
                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                                     className: "flex md:hidden justify-center items-center my-30px",
                                     children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["default"], {
-                                        src: "/img/hero/me.png",
+                                        src: "/img/hero/myphoto.png",
                                         width: 437,
                                         height: 475,
                                         alt: "banner image",
--- .next/static/chunks/_f1cb53._.js
+++ .next/static/chunks/_f1cb53._.js
@@ -1292,7 +1292,7 @@ const Hero = ()=>{
                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                                     className: "flex md:hidden justify-center items-center my-30px",
                                     children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
-                                        src: "/img/hero/me.png",
+                                        src: "/img/hero/myphoto.png",
                                         width: 437,
                                         height: 475,
                                         alt: "banner image",
